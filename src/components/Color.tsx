83f7708e1bbed816db2d715d1a8c49d0b497f0d0
ten66 <ikesho.1217@gmail.com>
2022-01-12 14:49:07 +0900

    引数のpropsをonChangeに変更

--- src/components/Color.tsx
+++ src/components/Color.tsx
@@ -4,12 +4,12 @@ type Props = {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
 }
 
-const Color: React.VFC<Props> = (props) => (
+const Color: React.VFC<Props> = ({ onChange }) => (
   <div>
     <label>Color </label>
     <input 
       type="color"
-      onChange={props.onChange}
+      onChange={onChange}
     />
   </div>
 )
